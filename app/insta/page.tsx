853ae9916786cd9ab0aa8de98705cd2075acf6de
chw07-coding <hunywoo3@naver.com>
2024-11-10 15:53:46 +0900

    todays web study

---
+++ app/insta/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { MessageCircleHeart } from 'lucide-react';
+import { MailCheck } from 'lucide-react';
+export default function Home() {
+  return (
+      <div className="flex justify-center items-center w-full h-[100dvh] bg-violet-100">
+        <div className="max-w-sm w-full h-[100dvh] bg-white">
+          <div className="flex justify-between justify-between bg-red-500 px-2">
+            <span className="font-bold border-4 border-blue-500">Instagram</span>
+            <div className='flex items-center gap-2'>
+              <button onClick={() =>mch()}>
+                <MessageCircleHeart className='w-6 h-6'/>
+              </button>
+              <button>
+                <MailCheck className='w-6 h-6'/>
+              </button> 
+            </div>
+          </div>
+        </div>   
+      </div>    
+  );
+}
+
+function mch(){
+  // console.log("MessageCircleHeart Button is pushed")
+  // alert("umjunsick")
+  // alert(prompt("id:"))//
+  confirm("yes or no!")
+}
--- app/page.tsx
+++ app/page.tsx
@@ -22,8 +22,8 @@ type Tparams={
 
 function Mybutton(params:Tparams){
   return(
-    <div className="bg-red-500">
-      <button>i am {params.name}. my nickname is {params.nickname} i am {params.today.getDate()} </button>
+    <div className="bg-indigo-500 text-xl w-[200px] h-[100px]" style ={{backgroundColor: "rgb(79 70 229)"}}>
+      i am {params.name}. my nickname is {params.nickname}.{} i am {params.today.getDate()}
     </div>
   )
    
